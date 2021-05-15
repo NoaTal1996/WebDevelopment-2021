313c37aa2fae085a8a9b45ecf640eda0fe12a333
NoaTal1996 <62711733+NoaTal1996@users.noreply.github.com>
2021-05-16 00:13:29 +0300

    Add files via upload

---
+++ assignment1-noaai/demo.js
@@ -0,0 +1,16 @@
+
+const funSumbit=()=>{
+   setTimeout(100)
+   clear();
+}
+const clear=()=>{
+    document.getElementById("msgSmile").value ="";
+    document.getElementById("fname").value ="";
+    document.getElementById("lname").value ="";
+    document.getElementById("mail").value ="";
+
+    
+}
+
+
+
